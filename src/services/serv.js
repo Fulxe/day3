fa906df11477ef5c2f6c52e784651238106a9b60
Fulxe <muruuuu8@gmail.com>
2022-11-22 22:49:09 +0800

    nemeltuud

--- src/services/serv.js
+++ src/services/serv.js
@@ -1,17 +1,17 @@
-import React from "react"
-import "./serv.css"
+import React from "react";
+import "./serv.css";
 
-
-function Serv () {
-    return (
-        <div className="nuur">
-            <div className="tolgoi">
-                <h1>Our</h1>
-                <h1>Services</h1>
-            </div>
-            <div></div>
+function Serv() {
+  return (
+    <div className="nuur">
+      <div className="center">
+        <div className="tolgoi">
+          <h1>Our Services</h1>
         </div>
-    )
+        <div className="tsej"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Serv;
