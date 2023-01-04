f3c244a523ee6d70390821000f3ae94cf6f969de
Fulxe <muruuuu8@gmail.com>
2023-01-04 18:12:07 +0800

    check

--- src/login/log.js
+++ src/login/log.js
@@ -1,6 +1,26 @@
 import "./log.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [firstName, setfirstName] = useState([]);
+  const [password, setlastName] = useState([]);
+
+  const create = () => {
+    axios
+      .get("http://localhost:2000", {
+        firstName: firstName,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.log(error)
+      }
+      );
+  };
+
   return (
     <div>
       <div className="main">
@@ -8,13 +28,21 @@ function Login() {
           <form autocomplete="off">
             <h2>Log in</h2>
             <div class="inputBox">
-              <input type="text" required="required" />
+              <input
+                type="firstName"
+                required="required"
+                onChange={(e) => setfirstName(e.target.value)}
+              />
               <span>Userame</span>
               <i></i>
             </div>
             <div class="inputBox">
-              <input type="password" required="required" />
-              <span>Password</span>
+              <input
+                type="lastName"
+                required="required"
+                onChange={(e) => setlastName(e.target.value)}
+              />
+              <span>lastName</span>
               <i></i>
             </div>
             <div class="links">
